df68fc7ea09a9a2a80d84f8ef9a9c1be5fbe7fd6
kayoxity <austinkispotta@gmail.com>
2018-10-25 04:23:40 +0530

    Added login

--- index.js
+++ index.js
@@ -1,16 +1,81 @@
-const express = require('express');
-const app = express();
+var express = require('express'),
+    app = express(),
+    bodyParser = require('body-parser'),
+    mongoose = require('mongoose'),
+    passport = require('passport'),
+    localStrategy = require('passport-local'),
+    passportLocalMongoose = require('passport-local-mongoose'),
+    User = require('./models/user');
 
-var myLogger = (req, res, next) => {
-    console.log('LOGGED')
-    next()
-  }
-  
-  app.use(myLogger);
+mongoose.connect("mongodb://localhost/test1", { useNewUrlParser: true });
+
+app.use(bodyParser.urlencoded({extended : true}));
+app.use(express.static('public'));
+app.set('view engine','ejs');
+
+
+app.use(require('express-session')({
+    secret : "Kayoxity is pro",
+    resave : false,
+    saveUninitialized : false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new localStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
+function isLoggedIn(req,res,next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect('/login');
+}
 
 app.get('/', (req,res) => {
-    res.send('Hello World');
-    console.log('Hello World');
+    res.render('index');
+});
+
+//Login route
+app.get('/login', (req,res) => {
+    res.render('login');
+});
+
+app.post('/login',passport.authenticate('local', {
+    successRedirect : '/home',
+    failureRedirect : '/login'
+}), (req,res) => {
+});
+
+//Logout route
+app.get('/logout',(req,res) => {
+    req.logout();
+    res.redirect('/');
+})
+
+//Register route
+app.get('/register', (req,res) => {
+    res.render('register');
+});
+
+app.post('/register', (req,res) => {
+    req.body.username;
+    req.body.password;
+    User.register(new User({username : req.body.username}), req.body.password , function(err,user) {
+        if(err){
+            console.log(err);
+            return res.render('register');
+        }
+        passport.authenticate('local')(req,res,function() {
+            res.redirect('index');
+        });
+    });
+});
+
+app.get('/home' ,isLoggedIn, (req,res) => {
+    res.render('home');
 });
 
 
